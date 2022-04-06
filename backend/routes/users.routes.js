3311eb33c9a728c44796503d3cb19923221b6ee5
tony-trung-nguyen <tnynguyen@ucdavis.edu>
2022-04-05 22:42:05 -0700

    Combined all users routes into one folder

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,13 +1,11 @@
-// create PUT user data
-
-import express from 'express'
-import User from '../models/users.model.js';
+import express from 'express';
+import User from '../models/users.model.js'; 
 
 const router = express.Router();
 
 function getDetailsFromRequest(req) {
     const name = req.body.name;
-    const email = req.body.email; 
+    const email = req.body.email;
     const collectedIDs = req.body.collectedIDs;
     const favoriteID = req.body.favoriteID;
     const huntStreak = req.body.huntStreak;
@@ -15,6 +13,36 @@ function getDetailsFromRequest(req) {
     return {name, email, collectedIDs, favoriteID, huntStreak};
 }
 
+// GET request
+router.route("/get_user/:id").get((req, res) => {
+    User.findById(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json("Error: " + err))
+});
+
+// POST request (create)
+router.route('/add_user').post((req, res) => { 
+    const name = req.body.name;
+    const email = req.body.email;
+    const collectedIDs = req.body.collectedIDs;
+    const favoriteID = req.body.favoriteID;
+    const huntStreak = req.body.huntStreak;
+
+    const newUser = new User({
+        name,
+        email,
+        collectedIDs,
+        favoriteID,
+        huntStreak
+    });
+
+    newUser.save()
+        .then(() => res.json(newUser))
+        .catch(err => res.status(400).json('Error: ' + err));
+    return req, res;
+});
+
+// PUT request (user data)
 router.route("/put/:id").put((req, res) => {
     User.findById(req.params.id)
         .then(user => {
