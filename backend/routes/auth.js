700e7c5619bf2b37c0940422cb136188e62b0bbd
tony-trung-nguyen <tnynguyen@ucdavis.edu>
2022-04-08 16:19:08 -0700

    Merge FAM-1

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,26 @@
 import express from 'express'
 import passport from 'passport'
+
 const router = express.Router()
 
-
-
-router.get('/auth/google',
+router.get('/google',
   passport.authenticate('google', { scope:
       [ 'email', 'profile' ] }
 ));
+
 router.get(
-    "/auth/google/callback",
+    "/google/callback",
     passport.authenticate("google", {
-      // successRedirect: "/auth/success",
-      failureRedirect: "failureURL",
+      	// successRedirect: "/auth/success",
+      	failureRedirect: "failureURL",
     }),
     function (req, res, next) {
-      console.log("second redirect", req.user);
-      res.redirect("http:localhost:19006/");
+      	console.log("second redirect", req.user);
+      	//res.redirect("http://localhost:19006/");
+        res.redirect("http://localhost:19006/" + req.user);
     }
 );
+
 
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,42 @@
 import express from 'express';
 import mongoose from 'mongoose'; 
+import cors from 'cors';
 import dotenv from 'dotenv'; 
-import usersRouter from './routes/users.route.js';
+import usersRouter from './routes/users.route.js'
+import authRouter from './routes/auth.js'
 import bodyParser from 'body-parser'; 
+import passport from 'passport';
+import session from 'express-session';
+import passportConfig from "./passport.js";
 
 const app = express();
 const port = 5000;
 
 dotenv.config(); //loads variables from env
+app.use(cors()); //https://daveceddia.com/access-control-allow-origin-cors-errors-in-react-express/
+
+passportConfig(passport);
+
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false //dont create session until stored
+    })
+);
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(bodyParser.json());
-app.use('/users', usersRouter);
+
+//routes
+app.use('/users', usersRouter)
+app.use('/auth', authRouter)
 
 
+// Setting up port and server 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
@@ -24,3 +49,5 @@ const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("MongoDB database connection established successfully.");
 });
+
+export default port;
