c677f4005b618ba5dd715f43250af2639aba052a
Alp <aguler@ucdavis.edu>
2022-04-09 17:20:55 -0700

    wrote cows schema

---
+++ backend/models/cows.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'; 
+
+const Schema = mongoose.Schema;
+
+const cowsSchema = new Schema(
+    {
+      S3link: { type: [String], required: true },
+      layersID: { type: [String], required: true },
+      location: { type: [{lat: [Number], lon: [Number], required: true }]}, 
+      collectedDate: { type: [Date], required: true },
+      name: { type: [String], required: true },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  const Cow = mongoose.model("Cows", cowsSchema);
+  export default Cow; 
