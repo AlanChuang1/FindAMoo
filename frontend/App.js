eeb8cbf3787b7747bc4bff1d72f105c231ddc693
alecl303 <alecliang303@gmail.com>
2022-04-21 15:39:48 -0700

    FAM-1-V2: not working android

--- frontend/App.js
+++ frontend/App.js
@@ -1,57 +1,14 @@
 import { StyleSheet, Text, TextInput, Alert, View, SafeAreaView, Button, useWindowDimensions } from 'react-native';
 import axios from 'axios';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
-import Google from "./GoogleLogin.js"
-
-const Separator = () => (
-	<View style={styles.separator} />
-);
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		marginHorizontal: 16,
-	},
-	title: {
-		textAlign: 'center',
-		marginVertical: 8,
-	},
-	fixToText: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-	separator: {
-		marginVertical: 8,
-		borderBottomColor: '#737373',
-		borderBottomWidth: StyleSheet.hairlineWidth,
-	},
-});
-
-let GetUserId = null; 
+import GoogleLogin from "./screens/GoogleLogin.js"
 
 const iAxios = axios.create({
 	baseURL: "http://localhost:5000",
 	timeout: 1000
 })
-
-const getCall = () => {
-	console.log("Trying GET...");
-	if (GetUserId != null) {
-		axios.get('http://localhost:5000/users/get_user/' + GetUserId).then(
-		(response) => {
-			console.log(response.status, response.statusText, response.data); 
-		}
-		).catch(
-			(response) => {
-				console.log("Request failed."); 
-			}
-		)
-	} else {
-		console.log("Please enter an ID first."); 
-	}
-}
 
 // Opens a window to backend (localhost:5000) to call google auth api and to login
 // TODO: after logining in, get certificate and store in asyncstorage to show they are logged in
@@ -60,35 +17,6 @@ const loginAuth = async () => {
 	console.log("Trying GoogleAuth...");
 	window.open('http://localhost:5000/auth/google', '__self');
 }
-
-const getStatus = () => {
-  	axios.get('http://localhost:5000/auth/success').then(
-    	(response) => {
-      		console.log(response.status, response.statusText, response.data, response.json); 
-    	}
-  	).catch(
-		(response) => {
-      		console.log("Request failed."); 
-    	}
-  )
-  //localStorage.setItem('name', 'john');
-}
-
-
-// Not being used currently 
-const fetchAuthUser = async () => {
-    const response = await axios
-        .get("http://localhost:5000/auth/user")
-        .catch((err) => {
-            console.log("Not properlly authenticated");
-        });
-
-    if (response && response.data){
-        console.log("User : ", response.data);
-    }
-    console.log("done");
-}
-  
 
 // Testing if saved currently to storage
 const saveCredentials = async (credentials) => {
@@ -105,32 +33,20 @@ const checkCrendentials = async () => {
 }
 
 
-export default function App() {
-	Google();
-  	// return (
-	// 	<NavigationContainer>
-	// 		<SafeAreaView style={styles.container}>
-	// 		<View>
-	// 			<Button title="LOGIN" onPress={() => loginAuth() } />
-	// 			<Separator/>
-	// 			<Button title="GET" onPress={() => { getCall() }}/>
-	// 			<TextInput
-	// 				style={{
-	// 					height: 40, 
-	// 					borderColor: 'gray', 
-	// 					borderWidth: 1
-	// 				}}
-	// 				placeholder="ID here"
-	// 				onFocus={() => {
-	// 					GetUserId = null; 
-	// 				}}
-	// 				onChangeText = {(newText) => {
-	// 					GetUserId = newText; 
-	// 				}}
-	// 			/>
-	// 			<Button title="Add to async" onPress={() => { saveCredentials("testing") }}/>
-	// 		</View>
-	// 		</SafeAreaView>
-	// 	</NavigationContainer>
-	// );
-}
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+	return (
+		<NavigationContainer>
+		<Stack.Navigator>
+			<Stack.Screen
+			name="Home"
+			component={GoogleLogin}
+			options={{ title: 'Welcome' }}
+			/>
+		</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
---
+++ frontend/screens/GoogleLogin.js
@@ -0,0 +1,43 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import * as React from 'react';
+import { StyleSheet, Text, TextInput, Alert, View, SafeAreaView, Button, useWindowDimensions } from 'react-native';
+import axios from 'axios';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function GoogleLogin() {
+	const [accesstoken, setAccessToken] = React.useState();
+
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		expoClientId: '805957129584-bstgfdmvh6sh1ks4tbjrpi57th2afm4p.apps.googleusercontent.com',
+		iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+		androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+		webClientId:  '805957129584-bstgfdmvh6sh1ks4tbjrpi57th2afm4p.apps.googleusercontent.com',
+	});
+
+	React.useEffect(() => {
+		if (response?.type === 'success') {
+			setAccessToken(response.authentication.accessToken);
+		}
+	}, [response]);
+
+	async function getUserData(){
+		let response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+			method: 'GET',
+			headers: {
+			  Authorization: `Bearer ${accesstoken}`,
+			},
+		});
+		return response.data;
+	}
+	return (
+		<Button
+		disabled={!request}
+		title="Login"
+		onPress={() => {
+			promptAsync();
+		}}
+		/>
+	);
+}
