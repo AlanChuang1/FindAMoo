4815180e064311fdc7d3dead0442db72265a2f6d
NathanUCD <nldcruz@ucdavis.edu>
2022-05-25 17:53:51 -0700

    Separated MapPage and Map styles

--- frontend/components/MapPage.js
+++ frontend/components/MapPage.js
@@ -2,7 +2,7 @@ import StatusBar from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps'; 
-import MapStyle from './css/MapPage.style.js'; 
+import MapStyle from './css/maps.style.js'; 
 
 export default function MapPage() {
   return (
